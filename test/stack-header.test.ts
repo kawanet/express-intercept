8e0dca55e0117758dcec12c9a3d8880d1dd8df3a
kawanet <u-suke@kawa.net>
2020-05-13 00:39:23 +0900

    stackRequestHeader() test

---
+++ test/stack-header.test.ts
@@ -0,0 +1,38 @@
+#!/usr/bin/env mocha -R spec
+
+import * as express from "express";
+
+import {responseHandler} from "../lib/express-intercept";
+import {middlewareTest} from "./lib/middleware-test";
+import {stackRequestHeader} from "./lib/stack-request-header";
+
+const TITLE = __filename.split("/").pop();
+
+describe(TITLE, () => {
+    const addResponseHeader = (key: string) => responseHandler().interceptStream((upstream, req, res) => {
+        res.setHeader(key, (req.header("x-foo") || "---") + "/" + (req.header("x-bar") || "---"));
+        return upstream;
+    });
+
+    it("stackRequestHeader()", async () => {
+        const app = express();
+        app.use(addResponseHeader("x-ret1"));
+        app.use(stackRequestHeader({"x-foo": "FOO"}));
+        app.use(addResponseHeader("x-ret2"));
+        app.use(stackRequestHeader({"x-bar": "BAR"}));
+        app.use(addResponseHeader("x-ret3"));
+        app.use(stackRequestHeader({"x-foo": "BAZ"}));
+        app.use(addResponseHeader("x-ret4"));
+        app.use((req, res) => res.send("OK"));
+
+        await middlewareTest(app)
+            .get("/")
+            .set({"x-bar": "QUX"})
+            .expect(200)
+            .expect("x-ret1", "---/QUX")
+            .expect("x-ret2", "FOO/QUX")
+            .expect("x-ret3", "FOO/BAR")
+            .expect("x-ret4", "BAZ/BAR")
+            .expect("OK");
+    });
+});
